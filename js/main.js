dbca77331c620294db8e63a9c902db350c571589
vasilii <wasiok123@gmail.com>
2021-02-24 17:01:45 +0200

    little up date

--- js/main.js
+++ js/main.js
@@ -8,7 +8,31 @@ $(document).ready(function(){
 		prevArrow: document.querySelector('#inner__header__my__arrow__right'),
 		nextArrow: document.querySelector('#inner__header__my__arrow__left'),
 	});
-	/* Accardion */
-	$('.demo').beefup();
-	
+
+
+	$('.leftbox').hover(function () {
+		$('.rightbox').animate({ width: '45.5%' }, 1000)
+	}, function () {
+		$('.rightbox').animate({ width: '0.4%' }, 1000)
+	});
+
+	$('.leftbox_1').hover(function () {
+		$('.rightbox_1').animate({ width: '45.5%' }, 1000)
+	}, function () {
+		$('.rightbox_1').animate({ width: '0.4%' }, 1000)
+	});
+
+	$('.leftbox_2').hover(function () {
+		$('.rightbox_2').animate({ width: '45.5%' }, 1000)
+	}, function () {
+		$('.rightbox_2').animate({ width: '0.4%' }, 1000)
+	});
+
+	$('.leftbox_3').hover(function () {
+		$('.rightbox_3').animate({ width: '45.5%' }, 1000)
+	}, function () {
+		$('.rightbox_3').animate({ width: '0.4%' }, 1000)
+	});
 });
+
+
